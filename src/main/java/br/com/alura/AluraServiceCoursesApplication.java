54a5d5e988544af727aea15ac53e3e4e31b23670
Maria Francisca Soares Miranda <mariafranciscasoares77@gmail.com>
2024-04-20 00:52:32 -0300

    feat: Projeto gerado no spring initializr - SpringBoot 3.2.5 | Java 21

---
+++ src/main/java/br/com/alura/AluraServiceCoursesApplication.java
@@ -0,0 +1,13 @@
+package br.com.alura;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AluraServiceCoursesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AluraServiceCoursesApplication.class, args);
+    }
+
+}
